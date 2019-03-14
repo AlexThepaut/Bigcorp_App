e6c74c277d7bae400e548346f027caa2c9812211
Alex <alex.thepaut33@gmail.com>
2019-03-14 16:47:20 +0100

    TP 16 fix

--- src/test/java/com/training/spring/repository/DaoTestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.training.spring.repository;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({"com.training.spring.repository", "com.training.spring.utils"})
-public class DaoTestConfig {
-}
