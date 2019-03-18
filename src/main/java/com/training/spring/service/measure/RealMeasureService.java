d14bc6ecba396614f8655d84d13e8590b690abde
Alex <alex.thepaut33@gmail.com>
2019-03-18 14:31:41 +0100

    Résolution problème git

--- src/main/java/com/training/spring/service/measure/RealMeasureService.java
+++ src/main/java/com/training/spring/service/measure/RealMeasureService.java
@@ -4,26 +4,28 @@ import com.training.spring.config.properties.BigCorpApplicationProperties;
 import com.training.spring.model.Captor;
 import com.training.spring.model.Measure;
 import com.training.spring.model.MeasureStep;
+import com.training.spring.model.RealCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service("realMeasureService")
-public class RealMeasureService implements MeasureService {
-
+@Component
+public class RealMeasureService implements MeasureService<RealCaptor> {
     @Autowired
     private BigCorpApplicationProperties properties;
-
     @Override
-    public List<Measure> readMeasures(Captor captor, Instant start, Instant end, MeasureStep step) {
+    public List<Measure> readMeasures(RealCaptor captor, Instant start, Instant end,
+                                      MeasureStep step) {
         checkReadMeasuresAgrs(captor, start, end, step);
         List<Measure> measures = new ArrayList<>();
         Instant current = start;
         while(current.isBefore(end)){
-            measures.add(new Measure(current, properties.getMeasure().getDefaultReal(), captor));
+            measures.add(new Measure(current,
+                    properties.getMeasure().getDefaultFixed(), captor));
             current = current.plusSeconds(step.getDurationInSecondes());
         }
         return measures;
